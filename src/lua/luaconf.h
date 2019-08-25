1391a94d53297620edc84a01b73a9896e75f8fc7
brinkqiang <brink.qiang@gmail.com>
2019-08-25 17:25:05 +0800

    add

--- src/lua/luaconf.h
+++ src/lua/luaconf.h
@@ -208,9 +208,7 @@
 #define LUA_PATH_DEFAULT  \
         LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
         LUA_CDIR"?.lua;"  LUA_CDIR"?/init.lua;" \
-        "./?.lua;" "./?/init.lua;" "!/../?.lua" \
-        "./../script/?.lua" \
-
+        "./?.lua;" "./?/init.lua;" "!/../?.lua;" "./../script/?.lua" \
 
 #define LUA_CPATH_DEFAULT \
         LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
